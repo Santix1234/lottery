10c87dd0c914a55b749fb594ad62cf6d1f953e60
souravg77 <souravg77@users.noreply.github.com>
2025-06-12 02:36:32 +0000

    Updated Lottery contract tests to cover new data tracking features

--- blockchain/test/Lottery.ts
+++ blockchain/test/Lottery.ts
@@ -1,77 +1,75 @@
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { Lottery } from "../typechain-types/contracts/Lottery";
+import { Lottery, Lottery__factory } from "../typechain-types";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
-const payloadToEnterLottery = { value: ethers.utils.parseEther("0.01") };
-
-describe("Lottery", () => {
+describe("Lottery Contract", function () {
   let lottery: Lottery;
   let owner: SignerWithAddress;
   let player1: SignerWithAddress;
   let player2: SignerWithAddress;
-  let signers: SignerWithAddress[];
 
-  const setUpTest = async () => {
-    signers = await ethers.getSigners();
-    [owner, player1, player2] = signers;
-
-    const lotteryFactory = await ethers.getContractFactory("Lottery", owner);
-    lottery = await lotteryFactory.deploy();
-    await lottery.deployed();
-  };
-
-  beforeEach(setUpTest);
-
-  it("Should deploy the Lottery contract", async () => {
-    expect(lottery.address).to.not.equal(0);
+  beforeEach(async function () {
+    [owner, player1, player2] = await ethers.getSigners();
+    const LotteryFactory: Lottery__factory = await ethers.getContractFactory("Lottery");
+    lottery = await LotteryFactory.deploy();
   });
 
-  it("Should allow players to enter the lottery", async () => {
-    await lottery.connect(player1).enter(payloadToEnterLottery);
-    const players = await lottery.getPlayers();
+  describe("Lottery Round Details", function () {
+    it("should initialize first lottery round correctly", async function () {
+      const round = await lottery.getLotteryRoundDetails(1);
+      expect(round.id).to.equal(1);
+      expect(round.participants.length).to.equal(0);
+      expect(round.totalPot).to.equal(0);
+      expect(round.ticketPrice).to.equal(ethers.utils.parseEther("0.01"));
+    });
 
-    expect(players.length).to.equal(1);
-    expect(players[0]).to.equal(player1.address);
+    it("should track participants and total pot when players enter", async function () {
+      await lottery.connect(player1).enter({ value: ethers.utils.parseEther("0.01") });
+      await lottery.connect(player2).enter({ value: ethers.utils.parseEther("0.02") });
+
+      const round = await lottery.getLotteryRoundDetails(1);
+      expect(round.participants.length).to.equal(2);
+      expect(round.totalPot).to.equal(ethers.utils.parseEther("0.03"));
+    });
   });
 
-  it("Should not allow players to enter with less than 0.01 ether", async () => {
-    const enterLotteryTx = lottery
-      .connect(player1)
-      .enter({ value: ethers.utils.parseEther("0.009") });
+  describe("Lottery Round History", function () {
+    it("should update round details when a winner is picked", async function () {
+      // Simulating lottery round with participants
+      await lottery.connect(player1).enter({ value: ethers.utils.parseEther("0.01") });
+      await lottery.connect(player2).enter({ value: ethers.utils.parseEther("0.02") });
 
-    await expect(enterLotteryTx).to.be.revertedWith("Min amount is 0.01 ether");
+      // Mock fulfilling random words
+      // Note: In a real test, you'd need to mock VRF
+      await lottery.connect(owner).startPickingWinner();
+
+      const round = await lottery.getLotteryRoundDetails(1);
+      expect(round.endTime).to.not.equal(0);
+      expect(round.winner).to.not.equal(ethers.constants.AddressZero);
+    });
+
+    it("should increment lottery ID after each round", async function () {
+      const initialId = await lottery.getLotteryId();
+      
+      // Simulate lottery round
+      await lottery.connect(player1).enter({ value: ethers.utils.parseEther("0.01") });
+      await lottery.connect(owner).startPickingWinner();
+
+      const newId = await lottery.getLotteryId();
+      expect(newId).to.equal(initialId.add(1));
+    });
   });
 
-  // it("Should allow the owner to pick a winner", async () => {
-  //   await lottery.connect(player1).enter(payloadToEnterLottery);
-  //   await lottery.connect(player2).enter(payloadToEnterLottery);
+  describe("Error Handling", function () {
+    it("should prevent entering lottery before withdrawal period", async function () {
+      await lottery.connect(player1).enter({ value: ethers.utils.parseEther("0.01") });
+      await lottery.connect(owner).startPickingWinner();
 
-  //   const balanceBefore = await owner.getBalance();
-  //   const pickWinnerTx = await lottery.connect(owner).pickWinner();
-  //   const balanceAfter = await owner.getBalance();
-
-  //   await expect(pickWinnerTx).not.to.be.reverted;
-  //   expect(balanceAfter.lt(balanceBefore)).to.be.true;
-  // });
-
-  it("Should not allow non-owners to pick a winner", async () => {
-    await lottery.connect(player1).enter(payloadToEnterLottery);
-
-    const pickWinnerTx = lottery.connect(player1).startPickingWinner();
-
-    expect(pickWinnerTx).to.be.revertedWith("Ownable: caller is not the owner");
+      // Try to enter during withdrawal period
+      await expect(
+        lottery.connect(player2).enter({ value: ethers.utils.parseEther("0.01") })
+      ).to.be.revertedWith("Next lottery not started yet");
+    });
   });
-
-  // it("Should reset the lottery after picking a winner", async () => {
-  //   await lottery.connect(player1).enter(payloadToEnterLottery);
-  //   await lottery.connect(player2).enter(payloadToEnterLottery);
-  //   await lottery.pickWinner();
-
-  //   const players = await lottery.getPlayers();
-  //   const lotteryId = await lottery.getLotteryId();
-
-  //   expect(players.length).to.equal(0);
-  //   expect(lotteryId).to.equal(1);
-  // });
 });
